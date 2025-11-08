5fc9477f567bd7f11217956c96774c361d83fc9c
Grumpyjdnh <juandiegonh2003@gmail.com>
2025-11-08 04:20:35 +0100

    terminado primera columna

--- Cursus/Libft/ft_memmove.c
+++ Cursus/Libft/ft_memmove.c
@@ -6,7 +6,27 @@
 /*   By: juannune <juannune@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/06 04:51:04 by juannune          #+#    #+#             */
-/*   Updated: 2025/11/06 05:07:04 by juannune         ###   ########.fr       */
+/*   Updated: 2025/11/06 05:51:37 by juannune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	unsigned char	*d;
+	unsigned char	*s;
+
+	if (src == NULL && dst == NULL)
+		return (NULL);
+	d = (unsigned char *)dst;
+	s = (unsigned char *)src;
+	if (d >= s)
+	{
+		while (len--)
+			d[len] = s[len];
+	}
+	else
+		ft_memcpy(d, s, len);
+	return (dst);
+}
