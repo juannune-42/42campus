aa3d767875777072af9d125e35895fa0b3dd215d
Grumpyjdnh <juandiegonh2003@gmail.com>
2026-02-17 02:27:17 +0100

    ??

---
+++ Cursus/Exam_Rank_2/ft_strcspn.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcspn.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juannune <juannune@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/14 04:53:01 by juannune          #+#    #+#             */
+/*   Updated: 2026/02/14 05:04:25 by juannune         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include    <string.h>
+
+size_t	ft_strcspn(const char *s, const char *reject)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	while (s[i])
+	{
+		j = 0;
+		while (reject[j])
+		{
+			if (s[i] == reject[j])
+				return (i);
+			j++;
+		}
+		i++;
+	}
+	return (i);
+}
