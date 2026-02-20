1f6514f3e2c5fb45b2f2598fb265612394eb6f10
Grumpyjdnh <juandiegonh2003@gmail.com>
2026-02-20 02:27:51 +0100

    El pushsawp esta dificil, empezamos

---
+++ Cursus/Push_swap/push_swap.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main()
+{
+    printf("hello, world\n");
+    return (0);
+}
+
+using System;
+
+public class HelloWorld{
+    public static void main(){
+        console.Writeline("Hello world!");
+    }
+}
