e533a047668f8f1d9a090ace125eb0d4f1819b33
Grumpyjdnh <juandiegonh2003@gmail.com>
2026-05-17 14:33:46 +0200

    42

--- Cursus/Libft/ft_strcspn.c
+++ Cursus/Libft/ft_strcspn.c
@@ -6,11 +6,11 @@
 /*   By: juannune <juannune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/14 04:53:01 by juannune          #+#    #+#             */
-/*   Updated: 2026/02/17 02:31:12 by juannune         ###   ########.fr       */
+/*   Updated: 2026/05/08 14:14:13 by juannune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include    <string.h>
+#include <string.h>
 
 size_t	ft_strcspn(const char *s, const char *reject)
 {
