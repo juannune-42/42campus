422e824c075e8df2100c2f1dcc086e94ea09a474
Grumpyjdnh <juandiegonh2003@gmail.com>
2026-06-10 20:01:30 +0200

    Python Modulo 00 Terminado 1.0

--- Cursus/Python_Module/Python_Module_00/ex1/ft_garden_name.py
+++ Cursus/Python_Module/Python_Module_00/ex1/ft_garden_name.py
@@ -1,4 +1,4 @@
 def ft_garden_name():
-    name: str = input("Enter garden name:")
+    name = input("Enter garden name:")
     print("Garden:", name)
     print("Status: Growing well!")
--- Cursus/Python_Module/Python_Module_00/ex6/ft_count_harvest_iterative.py
+++ Cursus/Python_Module/Python_Module_00/ex6/ft_count_harvest_iterative.py
@@ -1,7 +1,7 @@
 def ft_count_harvest_iterative():
-    temp: int = 1
+    count: int = 1
     day: int = int(input("Days until harvest: "))
-    while day >= temp:
-        print("Day", temp)
-        temp += 1
+    while day >= count:
+        print("Day", count)
+        count += 1
     print("Harvest time!")
